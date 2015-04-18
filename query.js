954d6a206bafe5fb903ede675e38b4edb2ffd8d5
jdavis4 <Jasmine Davis>
2015-04-18 15:56:22 -0400

    Took updated lulu branch
    Should be able to use this update to test JSON file reading, etc.

--- query.js
+++ query.js
@@ -1,12 +1,19 @@
-jQuery(document).ready(function($) {
-  $.ajax({
-  url : "http://api.wunderground.com/api/8b1c1856ceead734/geolookup/conditions/q/IA/Cedar_Rapids.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  alert("Current temperature in " + location + " is: " + temp_f);
-  }
-  });
+var base_url = "http://api.wunderground.com/api/8b1c1856ceead734/geolookup/conditions/q/"
+var ext = ".json"
+
+$(function($) {
+  //Upon submit, submit an ajax request and return JSON object
+  $("#target").submit(function() {
+      zip = $("#loc").val();
+      console.log(zip);
+
+      query_url = base_url + zip + ext;
+
+      var results = $.getJSON(query_url);
+      console.log(results);
 });
+
+}); // end of document.ready
+
+
 
