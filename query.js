965277e7d0476840df1284a7e427f0bd970bb4fb
jdavis4 <Jasmine Davis>
2015-04-18 18:06:30 -0400

    Grabbed New Additions from Lulu

--- query.js
+++ query.js
@@ -1,28 +1,39 @@
 var base_url = "http://api.wunderground.com/api/8b1c1856ceead734/geolookup/conditions/q/"
 var ext = ".json"
+var results = "";
 
 $(function($) {
   //Upon submit, submit an ajax request and return JSON object
-  $("#target").submit(function() {
+  $("#zip").submit(function() {
       zip = $("#loc").val();
       console.log(zip);
 
       query_url = base_url + zip + ext;
-      console.log(query_url);
-      var results = $.getJSON(query_url);
-      console.log(results);
 
-      var info = JSON.stringify(results);
-      console.log(info);
-      console.log(info.response)
-      info = JSON.parse(results);
-      //console.log(info);
+      $.getJSON(query_url,parse_data);
+  
+});
+    $("#current").submit(function() {
+      query_url = base_url + "autoip" + ext;
 
-      document.getElementById('info').innerHTML = results.city +", "+ results.state +" "+ results.zip;
-      document.getElementById('display').innerHTML = info;
+    $.getJSON(query_url, parse_data);
 });
 
+
 }); // end of document.ready
+
+function parse_data(parsed_json) {
+        var location = parsed_json['location']['city'];
+        var temp_f = parsed_json['current_observation']['temp_f'];
+        var relative_humidity = parsed_json['current_observation']['relative_humidity'];
+        var precip_1hr_in = parsed_json['current_observation']['precip_1hr_in'];
+        var weather = parsed_json['current_observation']['weather'];
+        
+        results = {city:location, temp:temp_f, hum:relative_humidity, precip:precip_1hr_in, weath:weather}
+
+        console.log(results);
+        return results;
+      }
 
 
 
