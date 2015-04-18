163fa743920d9e8c507a1e97bc507b01705589d8
jdavis4 <Jasmine Davis>
2015-04-18 18:01:57 -0400

    llama fun fun fun
    issue I was working on here was resolved in ‘lulu’ branch

--- query.js
+++ query.js
@@ -8,9 +8,18 @@ $(function($) {
       console.log(zip);
 
       query_url = base_url + zip + ext;
-
+      console.log(query_url);
       var results = $.getJSON(query_url);
       console.log(results);
+
+      var info = JSON.stringify(results);
+      console.log(info);
+      console.log(info.response)
+      info = JSON.parse(results);
+      //console.log(info);
+
+      document.getElementById('info').innerHTML = results.city +", "+ results.state +" "+ results.zip;
+      document.getElementById('display').innerHTML = info;
 });
 
 }); // end of document.ready
